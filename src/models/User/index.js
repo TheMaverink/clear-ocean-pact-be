058252907e3ab17dcb423b11a0a4715750f25e4e
JulioMauro <Julio@thinkcreatedigital.com>
2020-08-21 23:03:19 +0100

    done User model

---
+++ src/models/User/index.js
@@ -0,0 +1,95 @@
+import mongoose, { Schema } from 'mongoose';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import bcrypt from 'bcrypt';
+
+dotenv.config({ path: '.env' });
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      trim: true,
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin:{
+      type: Boolean,
+      default: false,
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    profileImage: {
+      type: String,
+      required: false,
+    },
+    role: {
+      type: String,
+    },
+    entries: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Entry',
+      },
+    ],
+    settings: {
+      private: {
+        type: Boolean,
+        default:false
+      },
+  },
+  },
+  { timestamps: true }
+);
+
+userSchema.virtual('entriesCount').get(function () {
+  return this.entries.length;
+});
+
+userSchema.methods.generateJwtToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user.id.toString() }, process.env.JWT_SECRET);
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
+
+userSchema.methods.getPublicProfile = function () {
+  const user = this;
+  const userObj = user.toObject();
+  delete userObj.password;
+  delete userObj.tokens;
+  return userObj;
+};
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
