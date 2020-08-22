58076423337d64d510ef8bac51c4bfa1ed7dc256
JulioMauro <Julio@thinkcreatedigital.com>
2020-08-22 01:26:58 +0100

    defined entries routes

--- src/routes/entries/index.js
+++ src/routes/entries/index.js
@@ -2,7 +2,16 @@
 //GET entries/$entryId
 //PUT entries/$entryId -> types,
 //Delete entries/$entryId 
-//GET entries/all 
-//GET entries/$entryId
-//GET entries/$entryId
+
 //PUT entries/new -> entryImage, location, types, isPrivate 
+
+import { Router } from 'express'
+import auth from '@middlewares/auth';
+
+const router = Router()
+
+router.get('/all', auth, getAllEntries)
+router.get('/:id', auth, getEntry)
+router.put('/:id', auth, updateEntry)
+router.delete('/:id', auth, deleteEntry)
+router.post('/new' ,auth, addEntry)
