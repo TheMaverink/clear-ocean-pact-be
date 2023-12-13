1be4cffafe676423d1859e5fced04b1650d174f7
TheMaverink <juliomaurodev@gmail.com>
2023-12-13 11:47:04 +0000

    removed consts

--- src/consts/index.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config({ path: '.env' });
-
-// LOGO_IMG_URL =
-//   'https://' +
-//   process.env.AWS_BUCKET_NAME +
-//   '.s3.' +
-//   process.env.AWS_BUCKET_REGION +
-//   '.amazonaws.com/app-images/logo.png'
